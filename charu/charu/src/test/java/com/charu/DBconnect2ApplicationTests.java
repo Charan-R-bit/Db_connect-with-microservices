259288d70dbcbdb17b6d65533abb06fe9fc38364
Charan R <96077678+Charan-R-bit@users.noreply.github.com>
2023-04-19 23:32:43 +0530

    Add files via upload

---
+++ charu/charu/src/test/java/com/charu/DBconnect2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBconnect2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
